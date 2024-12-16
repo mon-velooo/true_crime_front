bfba30ea6e4306004bfd69cafe711c0455236457
MaximePETIT-code <contactmaximepetit@gmail.com>
2024-12-16 13:58:09 +0100

    chore(dep): add typescript config & fix file architecture

---
+++ nextjs-docker/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import type { Metadata } from 'next';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'True crime',
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <main>{children}</main>
+            </body>
+        </html>
+    );
+}
