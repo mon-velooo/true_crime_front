89317d44364a3a3adc28f913a4b9efb45eba27e3
samuelaudic <samuelaudic@gmail.com>
2024-12-18 21:17:45 +0100

    feat(barchart): dynamize vertical bar chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,81 @@
-'use client';
-import { CustomBarChart } from '@/components/charts/CustomBarChart';
-import { CustomKPIChart } from '@/components/charts/CustomKPIChart';
-import { CustomLineChart } from '@/components/charts/CustomLineChart';
-import { CustomPieChart } from '@/components/charts/CustomPieChart';
-import { CustomVerticalBarChart } from '@/components/charts/CustomVerticalBarChart';
-import { Container } from '@/components/layout/Container/Container';
-import Grid from '@/components/layout/Grid/Grid';
-import { Card } from '@/components/ui/card';
-import { ChartConfig } from '@/components/ui/chart';
-import { DateRangePicker } from '@/components/ui/DateRangePicker';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { fetchDistricts } from '@/services/districts/fetchDistricts';
-import { useQuery } from '@tanstack/react-query';
+"use client";
+import { CustomBarChart } from "@/components/charts/CustomBarChart";
+import { CustomKPIChart } from "@/components/charts/CustomKPIChart";
+import { CustomLineChart } from "@/components/charts/CustomLineChart";
+import { CustomPieChart } from "@/components/charts/CustomPieChart";
+import { CustomVerticalBarChart } from "@/components/charts/CustomVerticalBarChart";
+import { Container } from "@/components/layout/Container/Container";
+import Grid from "@/components/layout/Grid/Grid";
+import { Card } from "@/components/ui/card";
+import { ChartConfig } from "@/components/ui/chart";
+import { DateRangePicker } from "@/components/ui/DateRangePicker";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { fetchDistricts } from "@/services/districts/fetchDistricts";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
   const pieData = [
-    { label: 'chrome', value: 275, fill: 'hsl(var(--chart-1))' },
-    { label: 'safari', value: 200, fill: 'hsl(var(--chart-2))' },
-    { label: 'firefox', value: 287, fill: 'hsl(var(--chart-3))' },
-    { label: 'edge', value: 173, fill: 'hsl(var(--chart-4))' },
-    { label: 'other', value: 190, fill: 'hsl(var(--chart-5))' }
+    { label: "chrome", value: 275, fill: "hsl(var(--chart-1))" },
+    { label: "safari", value: 200, fill: "hsl(var(--chart-2))" },
+    { label: "firefox", value: 287, fill: "hsl(var(--chart-3))" },
+    { label: "edge", value: 173, fill: "hsl(var(--chart-4))" },
+    { label: "other", value: 190, fill: "hsl(var(--chart-5))" },
   ];
 
   const pieConfig = {
-    chrome: { label: 'Chrome', color: 'hsl(var(--chart-1))' },
-    safari: { label: 'Safari', color: 'hsl(var(--chart-2))' },
-    firefox: { label: 'Firefox', color: 'hsl(var(--chart-3))' },
-    edge: { label: 'Edge', color: 'hsl(var(--chart-4))' },
-    other: { label: 'Other', color: 'hsl(var(--chart-5))' }
+    chrome: { label: "Chrome", color: "hsl(var(--chart-1))" },
+    safari: { label: "Safari", color: "hsl(var(--chart-2))" },
+    firefox: { label: "Firefox", color: "hsl(var(--chart-3))" },
+    edge: { label: "Edge", color: "hsl(var(--chart-4))" },
+    other: { label: "Other", color: "hsl(var(--chart-5))" },
   };
 
   const data = [
-    { label: 'January', desktop: 186, mobile: 80 },
-    { label: 'February', desktop: 305, mobile: 200 },
-    { label: 'March', desktop: 237, mobile: 120 },
-    { label: 'April', desktop: 73, mobile: 190 },
-    { label: 'May', desktop: 209, mobile: 130 },
-    { label: 'June', desktop: 214, mobile: 140 }
+    { label: "January", desktop: 186, mobile: 80 },
+    { label: "February", desktop: 305, mobile: 200 },
+    { label: "March", desktop: 237, mobile: 120 },
+    { label: "April", desktop: 73, mobile: 190 },
+    { label: "May", desktop: 209, mobile: 130 },
+    { label: "June", desktop: 214, mobile: 140 },
   ];
+
+  const chartData = [
+    { month: "January", desktop: 186 },
+    { month: "February", desktop: 305 },
+    { month: "March", desktop: 237 },
+    { month: "April", desktop: 73 },
+    { month: "May", desktop: 209 },
+    { month: "June", desktop: 214 },
+  ];
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "hsl(var(--chart-2))",
+    },
+  } satisfies ChartConfig;
 
   const config = {
     desktop: {
-      label: 'Desktop',
-      color: 'hsl(var(--chart-1))'
+      label: "Desktop",
+      color: "hsl(var(--chart-1))",
     },
     mobile: {
-      label: 'Mobile',
-      color: 'hsl(var(--chart-2))'
-    }
+      label: "Mobile",
+      color: "hsl(var(--chart-2))",
+    },
   } satisfies ChartConfig;
 
   const { data: districts } = useQuery({
-    queryKey: ['districts'],
-    queryFn: fetchDistricts
+    queryKey: ["districts"],
+    queryFn: fetchDistricts,
   });
 
   return (
@@ -66,7 +89,7 @@ export default function Home() {
           cols={{
             mobile: 2,
             tablet: 2,
-            desktop: 2
+            desktop: 2,
           }}
           gap={2}
           className="pb-4"
@@ -79,9 +102,18 @@ export default function Home() {
             footerText="Showing total visitors for the last 6 months"
           />
 
-          <CustomVerticalBarChart title="Most dangerous neighborhoods" data={districts} config={config} />
+          <CustomVerticalBarChart
+            title="test"
+            description="test"
+            data={chartData}
+            config={chartConfig}
+          />
 
-          <CustomBarChart title="Most dangerous neighborhoods" data={districts} config={config} />
+          <CustomBarChart
+            title="Most dangerous neighborhoods"
+            data={districts}
+            config={config}
+          />
 
           <CustomPieChart
             title="Répartition des types de crimes"
