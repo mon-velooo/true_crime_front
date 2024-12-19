fdad974d912d84e1f4e2d53180c19e53ea965471
Samuel Audic <samuelaudic@MacBook-Air-de-Samuel.local>
2024-12-19 14:19:46 +0100

    refacto(home): remove useless dep

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
 "use client";
-import { CustomBarChart } from "@/components/charts/CustomBarChart";
 import { CustomKPIChart } from "@/components/charts/CustomKPIChart";
-import { CustomLineChart } from "@/components/charts/CustomLineChart";
 import { OffencesCrimesCountPieChart } from "@/components/charts/OffencesCrimesCountPieChart";
-import { CustomVerticalBarChart } from "@/components/charts/CustomVerticalBarChart";
 import { Container } from "@/components/layout/Container/Container";
 import Grid from "@/components/layout/Grid/Grid";
 import { Card } from "@/components/ui/card";
-import { ChartConfig } from "@/components/ui/chart";
 import { DateRangePicker } from "@/components/ui/DateRangePicker";
-import { fetchDistricts } from "@/services/districts/fetchDistricts";
-import { fetchOffencesCrimesCount } from "@/services/graphs/fetchOffencesCrimesCount";
-import { useQuery } from "@tanstack/react-query";
-import { off } from "process";
 import * as React from "react";
 import { DateRange } from "react-day-picker";
-import { addDays, format } from "date-fns";
+import { format } from "date-fns";
 
 export default function Home() {
   const start = new Date("03-05-2024");
@@ -24,22 +16,6 @@ export default function Home() {
     from: start,
     to: today
   });
-
-  const chartData = [
-    { month: "January", desktop: 186 },
-    { month: "February", desktop: 305 },
-    { month: "March", desktop: 237 },
-    { month: "April", desktop: 73 },
-    { month: "May", desktop: 209 },
-    { month: "June", desktop: 214 },
-  ];
-
-  const chartConfig = {
-    desktop: {
-      label: "Desktop",
-      color: "hsl(var(--chart-2))",
-    },
-  } satisfies ChartConfig;
 
   const handleDateChange = (range: DateRange) => {
     setDateRange(range);
@@ -66,14 +42,6 @@ export default function Home() {
           gap={2}
           className="pb-4"
         >
-
-          <CustomVerticalBarChart
-            title="test"
-            description="test"
-            data={chartData}
-            config={chartConfig}
-          />
-
           <OffencesCrimesCountPieChart
             title="Breakdown of crime types"
             description={`${formatDate(dateRange?.from)} - ${formatDate(dateRange?.to)}`}
