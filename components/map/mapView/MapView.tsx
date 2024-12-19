052b95c98d63903f1917c7771db4340c645e474b
MaximePETIT-code <contactmaximepetit@gmail.com>
2024-12-19 23:49:08 +0100

    feat(map): enhance map with geocoding & fly to

--- components/map/mapView/MapView.tsx
+++ components/map/mapView/MapView.tsx
@@ -1,10 +1,13 @@
-'use client';
-import React from 'react';
-import Map, { FullscreenControl, GeolocateControl } from 'react-map-gl';
-import { config } from '@/lib/config';
-import MapMaker from './MapMaker';
-import type { Viewport, Crime, CrimesResponse } from '@/types/crimes';
-import 'mapbox-gl/dist/mapbox-gl.css';
+"use client";
+import React, { useCallback } from "react";
+import { useEffect, useRef } from "react";
+import Map, { FullscreenControl, GeolocateControl } from "react-map-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import mapboxgl from "mapbox-gl";
+import { config } from "@/lib/config";
+import type { Viewport, Crime, CrimesResponse } from "@/types/crimes";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 interface MapViewProps {
   className: string;
@@ -13,6 +16,7 @@ interface MapViewProps {
   crimes: CrimesResponse;
   selectedCrime: Crime | null;
   hoveredCrime: Crime | null;
+  pins: React.ReactNode;
   onCrimeSelect: (crime: Crime | null) => void;
   onCrimeHover: (crime: Crime | null) => void;
 }
@@ -21,34 +25,62 @@ export default function MapView({
   className,
   viewport,
   setViewport,
-  crimes,
+  pins,
   selectedCrime,
-  hoveredCrime,
-  onCrimeSelect,
-  onCrimeHover
 }: MapViewProps) {
-  const nycBounds: [number, number, number, number] = [-74.25909, 40.477399, -73.700272, 40.917577];
+  const nycBounds: [number, number, number, number] = [
+    -74.25909, 40.477399, -73.700272, 40.917577,
+  ];
+  const mapRef = useRef(null);
+
+  const initializeGeocoder = useCallback(() => {
+    if (!mapRef.current) return;
+
+    const geocoder = new MapboxGeocoder({
+      accessToken: config.mapbox.accessToken,
+      mapboxgl: mapboxgl,
+      marker: false,
+      bbox: nycBounds,
+      placeholder: "Search in NYC",
+    });
+
+    mapRef.current.addControl(geocoder);
+  }, []);
+
+  useEffect(() => {
+    if (selectedCrime && mapRef.current) {
+      mapRef.current.flyTo({
+        center: [selectedCrime.longitude, selectedCrime.latitude],
+        zoom: 15,
+        duration: 2000,
+        essential: true,
+      });
+    }
+  }, [selectedCrime]);
 
   return (
     <div className={className}>
       <Map
+        ref={mapRef}
         initialViewState={viewport}
         onMove={(evt) => setViewport(evt.viewState)}
         mapStyle={config.mapbox.mapStyle}
         mapboxAccessToken={config.mapbox.accessToken}
         maxBounds={nycBounds}
-        style={{ borderRadius: '0.5rem' }}
+        onLoad={initializeGeocoder}
+        style={{ borderRadius: "0.5rem" }}
       >
-        <FullscreenControl position="top-left" />
-        <GeolocateControl position="top-left" fitBoundsOptions={{ maxZoom: 15 }} />
+        {/* <FullscreenControl position="top-left" />
+        <GeolocateControl position="top-left" fitBoundsOptions={{ maxZoom: 15 }} /> */}
 
-        <MapMaker
+        {/* <MapMaker
           crimes={crimes}
           selectedCrime={selectedCrime}
           hoveredCrime={hoveredCrime}
           onCrimeSelect={onCrimeSelect}
           onCrimeHover={onCrimeHover}
-        />
+        /> */}
+        {pins}
       </Map>
     </div>
   );
