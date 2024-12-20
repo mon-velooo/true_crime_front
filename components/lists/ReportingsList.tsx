248c0f6536a75f44d0a9cff77123e0e383cd350c
Maxence Nho <78363268+PiKwaii93@users.noreply.github.com>
2024-12-20 14:46:41 +0100

    feat(socket) : socket reporting

--- components/lists/ReportingsList.tsx
+++ components/lists/ReportingsList.tsx
@@ -1,48 +1,58 @@
-import { useQuery } from "@tanstack/react-query";
+"use client";
 
+import { useQuery } from "@tanstack/react-query";
 import { ReportingData } from "@/types/reportings";
 import { fetchReportings } from "@/services/reportings/fetchReportings";
 import { ScrollArea } from "../ui/scroll-area";
-
-const reportings: ReportingData[] = [
-  {
-    id: "1",
-    latitude: 1.1,
-    longitude: 1.1,
-    status: "Reported",
-    description: "Robbery",
-  },
-  {
-    id: "2",
-    latitude: 1.1,
-    longitude: 1.1,
-    status: "Reported",
-    description: "Assault",
-  },
-  {
-    id: "3",
-    latitude: 1.1,
-    longitude: 1.1,
-    status: "Reported",
-    description: "Theft",
-  },
-];
+import { io } from "socket.io-client";
+import { useEffect, useState } from "react";
+import { useToast } from "@/hooks/use-toast";
 
 export const ReportingsList = () => {
-  const { data: reportings1, isLoading } = useQuery<ReportingData[]>({
+  const {toast} = useToast();
+  const SOCKET_URL = "http://localhost:5001"; 
+
+  const { data, isLoading } = useQuery<ReportingData[]>({
     queryKey: ["reportings"],
     queryFn: () => fetchReportings(),
   });
+
+  const [reportings, setReportings] = useState<ReportingData[]>(data || []);
+
+  useEffect(()=>{
+    if(data){
+      setReportings(data)
+    }
+  },[data])
+
+  useEffect(() => {
+
+    const socket = io(SOCKET_URL);
+
+    socket.on("newReporting", (newReporting: ReportingData) => {
+      setReportings((prevReportings) => [...prevReportings, newReporting]);
+      toast({
+        title: "Nouveau Reporting",
+        description: `${newReporting.description}`
+      })
+    });
+
+    return () => {
+      console.log("Cleaning up WebSocket connection...");
+      socket.off("newReporting");
+      socket.disconnect();
+    };
+  }, []); 
 
   return (
     <div className="w-full">
       <div className="relative">
         <ScrollArea className="h-[calc(100vh-510px)]">
           <div className="space-y-4">
-            {reportings1?.map((reporting) => (
+            {reportings?.map((reporting) => (
               <div
                 key={reporting.id}
-                className="p-4 rounded-md  transition-colors cursor-pointer border border-border-secondary"
+                className="p-4 rounded-md transition-colors cursor-pointer border border-border-secondary"
               >
                 <div className="space-y-3">
                   <div className="flex items-start justify-between gap-4">
